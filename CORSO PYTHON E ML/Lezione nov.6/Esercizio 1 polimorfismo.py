1b8b711a8e56e868f4d9434c7b6d8deef552c92c
domenicoiannizzi <domen10798@gmail.com>
2024-11-09 11:52:54 +0100

    Esercizio 1 polimorfismo con classe Metodo Bancario
    completo

--- CORSO PYTHON E ML/Lezione nov.6/Esercizio 1 polimorfismo.py
+++ CORSO PYTHON E ML/Lezione nov.6/Esercizio 1 polimorfismo.py
@@ -1,52 +1,117 @@
 class MetodoPagamento:
-    def __init__(self):
+    def __init__(self,saldo=0):
         self.__importo = None
+        self.__saldo = saldo
+
+    def get_saldo(self):
+        return self.__saldo
+    
+    def set_saldo(self,saldo):
+        self.__saldo=saldo
 
     def effettua_pagamento(self, importo):
-        self.__importo= importo
+        if importo <= self.__saldo:
+            self.__importo= importo
+            self.__saldo -= importo
 
     def get_importo(self):
         return self.__importo
 
 class CartaDiCredito(MetodoPagamento):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,saldo):
+        super().__init__(saldo)
+
+    def __test_credenziali(self,numero,cvv,scadenza):
+        return True
 
     def effettua_pagamento(self,importo):
-        super().effettua_pagamento(importo)
-        print(f"Pagamento di {self.get_importo()} effettuato con carta di credito.")
+        numero=int(input(("Inserisci numero carta :  ")))
+        scadenza=int(input("Inserisci anno di scadenza : "))
+        cvv=int(input("Inserisci cvv : "))
+
+        if self.__test_credenziali(numero,cvv,scadenza):
+            super().effettua_pagamento(importo)
+            print(f"Pagamento di {self.get_importo()} effettuato con carta di credito.")
+        else:
+            print("Credenziali errate")
+
 
 class PayPal(MetodoPagamento):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,saldo):
+        super().__init__(saldo)
+
+    def __test_credenziali(email,psw):
+        return True
 
     def effettua_pagamento(self, importo):
-        super().effettua_pagamento(importo) 
-        print(f"Pagamento di {self.get_importo()} effettuato con Paypal")
+        email=input("Inserisci email :")
+        psw=input("Inserisci password : ")
+        if self.__test_credenziali(email,psw):
+            super().effettua_pagamento(importo) 
+            print(f"Pagamento di {self.get_importo()} effettuato con Paypal")
+
+        else:
+            print("Credenziali errate")
 
 class BonificoBancario(MetodoPagamento):
-    def __init__(self):
-        super().__init__()
+    def __init__(self,saldo):
+        super().__init__(saldo)
+    
+    def __test_credenziali(iban):
+        return True
 
     def effettua_pagamento(self, importo):
-        super().effettua_pagamento(importo) 
-        print(f"Pagamento di {self.get_importo()} effettuato con bonifico bancario.")
+        iban= int(input("Inserisci IBAN del conto bancario : "))
+
+        if self.__test_credenziali(iban):
+            super().effettua_pagamento(importo) 
+            print(f"Pagamento di {self.get_importo()} effettuato con bonifico bancario.")
+
+        else:
+            print("IBAN errato")
+
 
 
 class GestorePagamenti:
     def __init__(self):
         pass
 
-    def pagamento(self,modalità:MetodoPagamento,importo):
-        modalità.effettua_pagamento(importo)
+    def pagamento(self,modalita: MetodoPagamento,importo):
+       modalita.effettua_pagamento(importo)
 
-carta= CartaDiCredito()
-bonifico=BonificoBancario()
-paypal=PayPal()
 
-pagare=GestorePagamenti()
-pagare = GestorePagamenti()
-pagare.pagamento(carta, 25)
+saldo=int(input("inserisci saldo :"))
+carta = CartaDiCredito(saldo)
+paypal = PayPal(saldo)
+bonifico = BonificoBancario(saldo)
 
-pagare.pagamento(paypal, 50)
-pagare.pagamento(bonifico, 100)
+gestore=GestorePagamenti()
+
+while True:
+    print("Scegli un metodo di pagamento:")
+    print("1 - Carta di Credito")
+    print("2 - PayPal")
+    print("3 - Bonifico Bancario")
+    print("q per uscire")
+    scelta=input("Inserisci scelta da effettuare ")
+    
+    if scelta == "1":
+        metodo=carta
+
+    elif scelta == "2":
+        metodo=paypal
+
+    elif scelta == "3":
+        metodo = bonifico
+
+    elif scelta == "q":
+        print("Esci")
+        break
+
+    else:
+        print("Scelta non valida, riprova.")
+        continue
+
+    importo= float(input("inserisci importo : "))
+    gestore.pagamento(metodo,importo)
+    
