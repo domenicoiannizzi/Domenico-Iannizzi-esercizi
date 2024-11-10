248cc6ca8d7dae57da16934825cd75eed2ef72ba
domenicoiannizzi <domen10798@gmail.com>
2024-11-10 11:46:59 +0100

    Esercizio Zoo

--- CORSO PYTHON E ML/Lezione nov.5/esercizio ereditarietà animali zoo.py
+++ CORSO PYTHON E ML/Lezione nov.5/esercizio ereditarietà animali zoo.py
@@ -1,31 +1,79 @@
 class Animale:
-     def __init__(self, nome, eta):
+    def __init__(self, nome, eta):
         self.nome = nome
         self.eta = eta
 
-     def suono(self):
+    def suono(self):
          pass
 
-class Gattide(Animale):
+class Gatti(Animale):
     def __init__(self, nome, eta):
         super().__init__(nome, eta)
-        self.tipo=tipo
 
-    def aggiungi_gattide(gattide):
-        gattide=+1
-    
-    
+    def suono(self):
+        super().suono()
+        print("Miaoooooo !!")
+
+    def graffio(self):
+        print(" Ti graffio amico, ops !")
 
 
-class Giraffide(Animale):
+
+class Conigli(Animale):
     def __init__(self, nome, eta):
         super().__init__(nome, eta)
-        self.tipo=tipo
 
-    def aggiungi_(gattide):
-        gattide=+1
+    def suono(self):
+        print("Boh, e chi lo sa il nome del suono")
     
+    def carota(self):
+        print("Dammi una carota, please")
 
-class Bovini(Animale):
+class Delfino(Animale):
+    def __init__(self, nome, eta):
+        super().__init__(nome, eta)
+
+    def suono(self):
+        print("Fischia !")
     
+    def salta(self):
+        print("Guarda come emerge dall'acqua con un salto")
+
+
+
+class Zoo:
+    def __init__(self):
+        self.diz_animali={} 
+
+
+    def aggiungi(self,specie,animale):
+        if specie not in self.diz_animali:
+            self.diz_animali[specie]=[]
+        self.diz_animali[specie].append(animale)
+
+    def visualizza_zoo(self):
+        for specie,lista in self.diz_animali.items():
+            for animale in lista:
+                print(f"Specie: {specie}, nome:{animale.nome}, età: {animale.eta}, suono: ")
+                animale.suono()
+                if specie.lower() == "gatto":
+                    animale.graffio()
+                if specie.lower() == "coniglio":
+                    animale.carota()
+                if specie.lower() == "delfino":
+                    animale.salta()
+
+
+gatto1=Gatti("Federico",26)
+gatto2=Gatti("Mimmo",21)
+
+zoo=Zoo()
+zoo.aggiungi("gatto",gatto1)
+zoo.aggiungi("gatto",gatto2)
+
+zoo.visualizza_zoo()
+
+
+
+        
 
